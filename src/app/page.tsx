7ed283a2f841bb88aee3f233c001b2912f25add5
EmanoelCariolando <emanoelcariolando@gmail.com>
2025-03-25 20:26:58 -0300

    Filter add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { peopleList } from "@/data/people";
  
 const Page = () => {
   const devs = peopleList.filter(person => person.job === 'dev');
+  const frees = peopleList.filter(person => person.job === 'free');
 
  return (
   <div className="flex flex-col items-center justify-between text-black">
@@ -9,7 +10,10 @@ const Page = () => {
    <ul>
     <h2 className="text-gray-700 text-3xl">Desenvolvedores</h2>
     {devs.map(person => 
-    <li>{person.name} - {person.job} - {person.age}</li>)}
+    <li>{person.name} - {person.job}</li>)}
+    <h2 className="text-gray-700 text-3xl">frees</h2>
+    {frees.map(person => 
+    <li>{person.name} - {person.job}</li>)}
    </ul>
    }
   
--- src/data/people.ts
+++ src/data/people.ts
@@ -2,7 +2,7 @@
  
  export const peopleList: Person[] = 
  [
-  {name: 'joao', age: 20 , job: 'security'},
+  {name: 'joao', age: 20 , job: 'free'},
   {name: 'manel', age: 19 , job: 'dev'},
   {name: 'ana', age: 25 , job: 'police'},
   {name: 'paul', age: 40 , job: 'free'},
