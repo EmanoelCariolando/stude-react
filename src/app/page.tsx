4846e275ccaffd3d1a656b7bef7929171ca9d4ee
EmanoelCariolando <emanoelcariolando@gmail.com>
2025-03-25 21:58:39 -0300

    Hours Site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,39 @@
-import { peopleList } from "@/data/people";
- 
-const Page = () => {
-  const devs = peopleList.filter(person => person.job === 'dev');
-  const frees = peopleList.filter(person => person.job === 'free');
+"use client";
+import { useEffect } from "react";
 
- return (
-  <div className="flex flex-col items-center justify-between text-black">
-   {devs.length > 0 &&
-   <ul>
-    <h2 className="text-gray-700 text-3xl">Desenvolvedores</h2>
-    {devs.map(person => 
-    <li>{person.name} - {person.job}</li>)}
-    <h2 className="text-gray-700 text-3xl">frees</h2>
-    {frees.map(person => 
-    <li>{person.name} - {person.job}</li>)}
-   </ul>
-   }
+const Page = () => {
+  const fullTime = new Intl.DateTimeFormat('pt-BR', {
+    timeStyle: 'short',
+    hour12: false,
+  }).format();
   
+  let hour = new Date().getHours();
+  let greeting = ''
+  let backgroundStyle = '';
+ 
+  if (hour >= 0 && hour <= 12) {
+    greeting = 'Bom Dia';
+    backgroundStyle = "bg-gradient-to-r from-sky-500 to-indigo-500"
+  }else if (hour > 12 && hour < 18) {
+    greeting = 'Boa Tarde';
+    backgroundStyle = "bg-gradient-to-r from-yellow-300 to-orange-500";
+  } else if (hour >= 18 && hour < 24) {
+    greeting = 'Boa Noite';
+    backgroundStyle = "bg-gradient-to-r from-gray-800 to-blue-900";
+  }
+
+  useEffect(() => {
+    document.body.style.background = ""; // Remove o background do body se necessário
+  }, []);
+
+  return(
+  <div className={`w-screen h-screen flex flex-col justify-center items-center text-black 
+  ${backgroundStyle}`}>
+
+  <div className="text-9xl">{fullTime}</div>
+  <div className="text-3xl">{greeting}</div>
+
   </div>
 
-)};
+)}
 export default Page;
